47aa147831c031f6717a65e5aa371f6a43e4a38b
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-02-28 15:14:21 -0500

    Fixed conf.py for Read the Docs & updated Poetry metadata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,67 +1,45 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'Djangautomate'
-copyright = '2025, pr1m8'
-author = 'pr1m8'
-release = '0.1.0'
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))  # Adjust for the correct path
-
-#sys.path.insert(0, os.path.abspath('../../djangautomate'))  # Adjust for the correct path
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 import os
 import sys
 
+# -- Project Information -------------------------------------------------
+project = "DjangAutomate"
+copyright = "2025, pr1m8"
+author = "pr1m8"
+release = "0.1.0"  # Ensure this matches the package version
+
+# -- Path Configuration ---------------------------------------------------
 sys.path.insert(0, os.path.abspath('../../'))  # Ensure the module is found
 
+# -- Sphinx Extensions ----------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'autoapi.extension',  # Enables automatic API documentation
+    'sphinx.ext.autodoc',      # Auto-generate documentation from docstrings
+    'sphinx.ext.napoleon',     # Google-style docstrings support
+    'sphinx.ext.viewcode',     # Add links to source code
 ]
 
-# Point AutoAPI to the correct module directory
-autoapi_dirs = ['../../djangautomate']
+# -- Autodoc Settings -----------------------------------------------------
+autodoc_mock_imports = ["sqlalchemy", "django"]
+autodoc_member_order = "bysource"  # Keep function order same as source code
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "private-members": False,
+    "special-members": "__init__",
+    "show-inheritance": True,
+}
 
-# Ensure docstrings are properly extracted
-autoapi_options = [
-    'members',
-    'undoc-members',
-    'private-members',
-    'special-members',
-    'show-inheritance',
-    'show-module-summary',
-    'imported-members',
-]
-
-# Use Google-style docstrings
+# -- Google-style docstrings ----------------------------------------------
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 
-# Ensure Sphinx knows to treat .rst files properly
+# -- Source File Settings -------------------------------------------------
 source_suffix = ['.rst']
+master_doc = 'index'  # Ensure index.rst is the main file
 
-
+# -- HTML Output Settings -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-
+# -- Templates Path -------------------------------------------------------
 templates_path = ['_templates']
 exclude_patterns = []
-
-language = 'y'
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-#html_theme = 'alabaster'
-#html_static_path = ['_static']
