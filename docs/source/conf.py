1df4f90deef2197a86720582663be890b6be0489
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-02-28 15:50:47 -0500

    updated static and template paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,8 +38,8 @@ master_doc = 'index'  # Ensure index.rst is the main file
 
 # -- HTML Output Settings -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = ['./_static']
 
 # -- Templates Path -------------------------------------------------------
-templates_path = ['_templates']
+templates_path = ['./_templates']
 exclude_patterns = []
